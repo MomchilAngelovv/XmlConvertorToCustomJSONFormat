1d88b240f2621249cdcf3341388d0307c6f651de
MomchilAngelovv <momchilangelov0040@gmail.com>
2019-09-27 09:35:18 +0300

    Initial commit with basic functionality

---
+++ CustomXmlToJSONConvertor/CustomXmlToJSONConvertor/KeyValueTypeJson.cs
@@ -0,0 +1,10 @@
+﻿namespace XmlToJsonRigSoftware
+{
+    public class KeyValueTypeJson
+    {
+        public string PropertyName { get; set; }
+        public string DisplayName { get; set; }
+        public object Value { get; set; }
+        public short Type { get; set; } = 0;
+    }
+}
---
+++ CustomXmlToJSONConvertor/CustomXmlToJSONConvertor/Program.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace XmlToJsonRigSoftware
+{
+    class Program
+    {
+        private static readonly List<string> currentElements = new List<string>();
+        private static int counter = 0;
+        private static string lastNodeName = "";
+
+        static void Main()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"sample.xml");
+
+            StringBuilder sb = new StringBuilder();
+            TraverseElements(doc.ChildNodes, sb);
+
+            var result = "{" + sb.ToString().Remove(sb.ToString().LastIndexOf(',')) + "}";
+            var json = JsonConvert.DeserializeObject<object>(result);
+            Console.WriteLine(json);
+        }
+
+        private static void TraverseElements(XmlNodeList children, StringBuilder sb)
+        {
+            if (children.Count != 0)
+            {
+                for (int i = 0; i < children.Count; i++)
+                {
+                    if (children.Item(i).ChildNodes.Count != 0)
+                    {
+                        currentElements.Add(children.Item(i).Name);
+                        lastNodeName = children.Item(i).ParentNode?.Name;
+                        TraverseElements(children.Item(i).ChildNodes, sb);
+                    }
+                }
+
+                string value = children.Item(0).Value;
+
+                if (children.Item(0).NodeType != XmlNodeType.XmlDeclaration)
+                {
+                    if (!string.IsNullOrWhiteSpace(value))
+                    {
+                        string stringLine = ("\"" + string.Join(".", currentElements) + "\"" + ":" + "\"" + children.Item(0).Value + "\",");
+                        sb.AppendLine(stringLine);
+                    }
+
+                    if (currentElements.Count != 0)
+                    {
+                        currentElements.RemoveAt(currentElements.Count - 1);
+                        counter++;
+                    }
+
+                    if (currentElements.Count == counter && currentElements.Count != 0)
+                    {
+                        for (int j = 0; j < currentElements.Count - counter; j++)
+                        {
+                            currentElements.RemoveAt(currentElements.Count - 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
